a041c6d7ea5e3aeae468226503ac8f8f3f1d2442
ruiyaa <nini2525min@gmail.com>
2026-06-14 23:15:22 +0900

    Initial Commit

---
+++ illustration collector/illustration_collectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  illustration_collectorApp.swift
+//  illustration collector
+//
+//  Created by tanaka niko on 2026/06/14.
+//
+
+import SwiftUI
+
+@main
+struct illustration_collectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ illustration collectorTests/illustration_collectorTests.swift
@@ -0,0 +1,18 @@
+//
+//  illustration_collectorTests.swift
+//  illustration collectorTests
+//
+//  Created by tanaka niko on 2026/06/14.
+//
+
+import Testing
+
+struct illustration_collectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
